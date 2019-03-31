ffc906f8f06e86a34f048effd98665bacf706b28
atamuhiuldin <atamuhiuldin@gmail.com>
2019-03-31 23:44:55 +0500

    Updates

---
+++ precache-manifest.29137a03da81b85f154845d9ca1a5bd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DEPLOYTEST/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "567ca7364af8b2b35d0d",
+    "url": "/DEPLOYTEST/static/js/runtime~main.3f4610ad.js"
+  },
+  {
+    "revision": "973bc77eee8a6f7a3e77",
+    "url": "/DEPLOYTEST/static/js/main.586bf97b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/DEPLOYTEST/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "973bc77eee8a6f7a3e77",
+    "url": "/DEPLOYTEST/static/css/main.df184d2e.chunk.css"
+  },
+  {
+    "revision": "c155411499d60d61db7f40b545c178db",
+    "url": "/DEPLOYTEST/index.html"
+  }
+];
